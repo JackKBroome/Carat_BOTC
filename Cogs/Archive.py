a9c20e79907a64bf8574cd833ddeac20b65b4b3f
JackKBroome <87062606+JackKBroome@users.noreply.github.com>
2024-05-30 15:43:41 +0100

    Update Archive.py

--- Cogs/Archive.py
+++ Cogs/Archive.py
@@ -149,7 +149,6 @@ class Archive(commands.Cog):
         await ctx.send("1")
         archive_server = self.helper.bot.get_guild(1203126128693354516)
         await ctx.send("2")
-        
         Unique_role = nextcord.utils.get(archive_server.roles, name=Unique_role_name)
         await ctx.send("4")
         if Unique_role is None:

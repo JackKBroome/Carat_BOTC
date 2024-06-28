c1477d315affdea7cc80e47b2fac6c04d28d92e1
JackKBroome <87062606+JackKBroome@users.noreply.github.com>
2024-06-28 19:15:38 +0100

    Update Archive.py

--- Cogs/Archive.py
+++ Cogs/Archive.py
@@ -19,6 +19,10 @@ class ThreadList:
     private_to_archive: List[int] = field(default_factory=list)
     public_to_not_archive: List[int] = field(default_factory=list)
 
+class Game(commands.Cog):
+    def __init__(self, bot: commands.Bot, helper: utility.Helper):
+        self.bot = bot
+        self.helper = helper
 
 async def copy_history(target: nextcord.abc.Messageable, history) -> int:
     errors = 0
@@ -154,6 +158,118 @@ class Archive(commands.Cog):
         await utility.finish_processing(ctx)
 
     @commands.command()
+    async def ArchiveGame(self, ctx, game_number):
+        """Moves the game channel to the archive and creates a new empty channel for the next game.
+        Also makes the kibitz channel hidden from the public. Use after post-game discussion has concluded.
+        Do not remove the game number from the channel name until after archiving.
+        You will still be able to do so afterward."""
+        if self.helper.authorize_st_command(ctx.author, game_number):
+            # React on Approval
+            await utility.start_processing(ctx)
+
+            townsfolk_role = self.helper.Guild.default_role
+            st_role = self.helper.get_st_role(game_number)
+            game_channel = self.helper.get_game_channel(game_number)
+            if game_channel is None:
+                await utility.deny_command(ctx, "No game for that number found")
+                return
+            game_position = game_channel.position
+            game_channel_name = game_channel.name
+            archive_category = self.helper.ArchiveCategory
+            if len(archive_category.channels) == 50:
+                await utility.deny_command(ctx, "Archive category is full")
+                await game_channel.send(f"{self.helper.ModRole.mention} The archive category is full, so this channel "
+                                        f"cannot be archived")
+                return
+            if game_number[0] != "r":
+                new_channel = await game_channel.clone(reason="New Game")
+                await new_channel.edit(position=game_position, name=f"text-game-{game_number}", topic="")
+            # remove manage threads permission so future STs for the game number can't see private threads
+            st_permissions = game_channel.overwrites[st_role]
+            st_permissions.update(manage_threads=None)
+            await game_channel.set_permissions(st_role, overwrite=st_permissions)
+            for st in st_role.members:
+                if st in game_channel.overwrites:
+                    member_permissions = game_channel.overwrites[st]
+                    member_permissions.update(manage_threads=True)
+                    await game_channel.set_permissions(st, overwrite=member_permissions)
+                else:
+                    await game_channel.set_permissions(st, manage_threads=True)
+            await game_channel.edit(category=archive_category, name=str(game_channel_name) + " Archived on " + str(
+                strftime("%a, %d %b %Y %H %M %S ", gmtime())), topic="")
+
+            kibitz_channel = self.helper.get_kibitz_channel(game_number)
+            await kibitz_channel.set_permissions(townsfolk_role, view_channel=False)
+
+            channel_to_archive = ctx.message.channel
+            archive_server_id = 1203126128693354516
+            archive_server = self.helper.bot.get_guild(archive_server_id)
+            if archive_server is None:
+                await utility.dm_user(ctx.author, f"Was unable to find server with ID {archive_server_id}")
+                return
+            archive_channel = get(archive_server.channels, id=archive_channel_id)
+            if archive_channel is None:
+                archive_channel = await archive_server.create_text_channel(name="Temp Channel")
+                Channel_name = str(channel_to_archive.name) + "-" + str(member.display_name)
+                await archive_channel.edit(name=Channel_name)
+
+            access = self.helper.authorize_mod_command(ctx.author)
+            # Ivy Access
+            if access or ctx.author.id == ivy_id:
+                # React on Approval
+                await utility.start_processing(ctx)
+                Unique_role_name = str(member.id)
+                Unique_role = nextcord.utils.get(archive_server.roles, name=Unique_role_name)
+                if Unique_role is None:
+                    Unique_role = await archive_server.create_role(name=Unique_role_name)
+
+                channel_history = channel_to_archive.history(limit=None, oldest_first=True)
+                errors = await copy_history(archive_channel, channel_history)
+                for thread in channel_to_archive.threads:
+                    if thread.is_private() and (thread.parent.id not in self.threads_by_channel or
+                                                thread.id not in self.threads_by_channel[
+                                                    channel_to_archive.id].private_to_archive):
+                        try:
+                            archive_thread = await archive_channel.create_thread(
+                                name=str(thread.name),
+                                auto_archive_duration=4320,  # 3 days
+                                type=nextcord.ChannelType.private_thread,
+                                invitable=True,
+                                reason="Private Thread"
+                                )
+                            thread_history = thread.history(limit=None, oldest_first=True)
+                            errors += await copy_history(archive_thread, thread_history)
+                            continue
+                        except HTTPException:
+                            await archive_channel.send(f"Failed to create thread '{thread.name}'")
+                            continue
+                    elif (not thread.is_private()) and thread.parent.id in self.threads_by_channel and \
+                            thread.id in self.threads_by_channel[channel_to_archive.id].public_to_not_archive:
+                        continue
+                    try:
+                        archive_thread = await archive_channel.create_thread(name=thread.name,
+                                                                            type=nextcord.ChannelType.public_thread)
+                        thread_history = thread.history(limit=None, oldest_first=True)
+                        errors += await copy_history(archive_thread, thread_history)
+                    except HTTPException:
+                        await archive_channel.send(f"Failed to create thread '{thread.name}'")
+                        continue
+                await archive_channel.create_thread(name="Chat about the game", type=nextcord.ChannelType.public_thread)
+                await archive_channel.set_permissions(Unique_role, manage_threads=True)
+                await utility.finish_processing(ctx)
+                self.threads_by_channel.pop(channel_to_archive.id, None)
+                self.update_storage()
+                #await self.helper.log(f"{ctx.author.display_name} has run the OffServerArchive Command")
+
+            # React for completion
+            await utility.finish_processing(ctx)
+
+        else:
+            await utility.deny_command(ctx, "You are not the current ST for game " + game_number)
+
+        await self.helper.log(f"{ctx.author.mention} has run the ArchiveGame Command for Game {game_number}")
+
+    @commands.command()
     async def OffServerArchive(self, ctx: commands.Context, archive_server_id: int, member: nextcord.Member, archive_channel_id: int =0):
         """Copies the channel the message was sent in to the provided server and channel, message by message.
         Attachments may not be preserved if they are too large. Also creates a discussion thread at the end.
@@ -260,4 +376,3 @@ class Archive(commands.Cog):
 
 def setup(bot: commands.Bot):
     bot.add_cog(Archive(bot, utility.Helper(bot)))
-
